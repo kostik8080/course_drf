4a09bced46a47ef429994917ebcfb5a3ed147aea
Konstantin Onishchenko <kostik80_80@mail.ru>
2024-05-10 13:03:14 +0300

    создание проекта и создание модели пользователь

---
+++ users/models.py
@@ -0,0 +1,17 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+NULLABLE = {'null': True, 'blank': True}
+
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True, verbose_name='Почта')
+    phone = models.CharField(max_length=20, verbose_name='Номер телефона', **NULLABLE)
+    city = models.CharField(max_length=100, verbose_name='Город', **NULLABLE)
+    avatar = models.ImageField(upload_to='avatars', **NULLABLE)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+
