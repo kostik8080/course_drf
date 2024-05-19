cc93a97c9554a469689c34141df69d017f4519a3
Konstantin Onishchenko <kostik80_80@mail.ru>
2024-05-19 20:06:05 +0300

    Добавил в модель пользователь chat_id и через него уже происходит логика отправки сообщения в телеграм

--- habit/services.py
+++ habit/services.py
@@ -1,6 +1,10 @@
 import requests
 import logging
+
+from django.db.models import Q
+
 from config import settings
+from users.models import User
 
 logger = logging.getLogger(__name__)
 
@@ -17,11 +21,19 @@ def send_telegram(chat_id, message):
 
 
 def create_telegram_message(habit):
-    message = (f'Привет тебе, пора {habit.action}'
-               f' в {habit.place}.'
-               f'Ты сам захотел это сделать'
-               f' в {habit.place}.'
-               f'И давай не забывай, тебе всего надо'
-               f' на это {habit.time_to_complete} минут.')
-    send_telegram(settings.TELEGRAM_CHAT_ID, message)
-    logger.info(f"Уведомление {message} отправлено")
+    try:
+        user = User.objects.get(Q(email=habit.user.email))
+        chat_id = user.chat_id
+        if chat_id:
+            message = (f'Привет тебе, пора {habit.action}'
+                       f' в {habit.place}.'
+                       f'Ты сам захотел это сделать'
+                       f' в {habit.place}.'
+                       f'И давай не забывай, тебе всего надо'
+                       f' на это {habit.time_to_complete} минут.')
+            send_telegram(chat_id, message)
+            logger.info(f"Уведомление {message} отправлено")
+    except User.DoesNotExist:
+        logger.info(f"Пользователь с email {habit.user.email} не найден")
+
+
